6e1a0f4d96775a6f9cfb746ae178e588deb4bb55
Mosky <mosky.tw@gmail.com>
2013-05-22 18:00:26 +0800

    added draft of bricks.py

---
+++ bricks.py
@@ -0,0 +1,109 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# --- import ---
+
+import sys
+import struct
+
+try:
+  from OpenGL.GL import *
+  from OpenGL.GLU import *
+  from OpenGL.GLUT import *
+except:
+    print 'It needs PyOpenGL and OpenGL.'
+    sys.exit()
+
+# --- helpers ---
+
+def rgbhex(rgbhex):
+
+    rgbhex = rgbhex.strip('# ')
+
+    if len(rgbhex) == 3:
+        rgbhex = rgbhex[0]*2 + rgbhex[1]*2 + rgbhex[2]*2
+    if len(rgbhex) == 6:
+        return (int(rgbhex[ :2], 16)/255.0,
+                int(rgbhex[2:4], 16)/255.0,
+                int(rgbhex[4: ], 16)/255.0)
+
+def line(x=0, y=0, size=0, angle=0):
+
+    glPushMatrix()
+
+    glMatrixMode(GL_PROJECTION)
+    glTranslate(x, y, 0)
+    glRotate(angle, 0, 0, 1)
+    glScale(size, 1, 1)
+
+    glBegin(GL_LINES)
+    glVertex2f(0, 0)
+    glVertex2f(1, 0)
+    glEnd()
+
+    glPopMatrix()
+
+def square(x=0, y=0, size=0, angle=0):
+
+    glPushMatrix()
+
+    glMatrixMode(GL_PROJECTION)
+    glTranslate(x, y, 0)
+    glRotate(angle, 0, 0, 1)
+    glScale(size, size, 1)
+
+    glBegin(GL_QUADS)
+    glVertex2f(0, 0)
+    glVertex2f(0, 1)
+    glVertex2f(1, 1)
+    glVertex2f(1, 0)
+    glEnd()
+
+    glPopMatrix()
+
+# --- main ---
+
+WIDTH = 500
+HEIGHT = 500
+UNIT = 100
+
+def init():
+    glClearColor(0, 0, 0, 0)
+
+def display():
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    glColor(rgbhex('#00FF00'))
+
+    for x in range(0, WIDTH, UNIT):
+        line(x=x, size=HEIGHT, angle=90)
+
+    for y in range(0, HEIGHT, UNIT):
+        line(y=y, size=WIDTH)
+
+    glColor(rgbhex('#FF0000'))
+    square(x=UNIT, y=UNIT, size=UNIT, angle=30)
+
+    glFlush()
+
+def reshape(w, h):
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glViewport(0, 0, w, h)
+    gluOrtho2D(0.0, w, 0.0, h)
+
+def keyboard(key, x, y):
+    if key == 'q':
+        sys.exit(0)
+
+if __name__ == '__main__':
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+    glutInitWindowSize(WIDTH, HEIGHT)
+    glutInitWindowPosition(100, 100)
+    glutCreateWindow('Grid')
+    init()
+    glutReshapeFunc(reshape)
+    glutKeyboardFunc(keyboard)
+    glutDisplayFunc(display)
+    glutMainLoop()
